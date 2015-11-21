fb5c0660ca5f914859a264ac8d69aa943f54a7d5
IDSFN <nedalkouissi@gmail.com>
2015-11-21 18:44:11 -0500

    fixed some arrow styling behaviour

--- script.js
+++ script.js
@@ -35,3 +35,47 @@ if (window.matchMedia('only screen and (min-width: 800px)').matches) {
       initCanvas();
    });
 }
+
+
+/*==================================================
+=            fancy shit to the header               =
+==================================================*/
+var desktopWidth = window.matchMedia('only screen and (min-width: 512px)').matches;
+$(window).resize(function() {
+   desktopWidth = window.matchMedia('only screen and (min-width: 512px)').matches;
+
+   if (!desktopWidth) $('header').off();
+});
+
+if (desktopWidth) {
+   $('header').on({
+      // Show and hide nex and prev and down
+      mousemove: function(event) {
+         var mouseX = (event.pageX / screen.width) * 100;
+         var mouseY = (event.pageY / screen.height) * 100;
+         var left = $('.prev');
+         var right = $('.next');
+         var down = $('.circleDown');
+
+         if (mouseX <= 10)
+            left.css('opacity', 0.4);
+
+         else if (mouseX >= 60)
+            right.css('opacity', 0.4);
+
+         else if (mouseY >= 60)
+            down.css('opacity', 0.8);
+
+         else {
+            left.css('opacity', 0);
+            right.css('opacity', 0);
+            down.css('opacity', 0);
+         }
+      },
+      // Add a flash effect to the header
+      click: function() {
+         (new Audio('sounds/capture.wav')).play();
+         $('.flash').fadeIn(30).fadeOut(100);
+      }
+   });
+}
